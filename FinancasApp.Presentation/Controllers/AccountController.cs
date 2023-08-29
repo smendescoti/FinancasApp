a6a45e70c041177aa9890208e9e5b325870da650
smendescoti <sergio.coti@gmail.com>
2023-08-28 21:44:01 -0300

    feature: Cadastro de conta de usuário

--- FinancasApp.Presentation/Controllers/AccountController.cs
+++ FinancasApp.Presentation/Controllers/AccountController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FinancasApp.Data.Entities;
+using FinancasApp.Data.Helpers;
+using FinancasApp.Data.Repositories;
+using FinancasApp.Presentation.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FinancasApp.Presentation.Controllers
 {
@@ -20,6 +24,57 @@ namespace FinancasApp.Presentation.Controllers
         /// </summary>
         public IActionResult Register()
         {
+            return View();
+        }
+
+        /// <summary>
+        /// Método para receber o submit POST da página /Account/Register
+        /// </summary>
+        [HttpPost]
+        public IActionResult Register(AccountRegisterViewModel model)
+        {
+            //verificar se todos os campos enviados passaram
+            //nas regras de validação do DataAnnotations
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    var usuarioRepository = new UsuarioRepository();
+
+                    //verificar se já existe um usuário cadastrado com o emaio informado
+                    if(usuarioRepository.GetByEmail(model.Email) != null)
+                    {
+                        TempData["MensagemAlerta"] = "O email informado já está cadastrado para outro usuário.";
+                    }
+                    else
+                    {
+                        //capturando os dados do usuário
+                        var usuario = new Usuario
+                        {
+                            Id = Guid.NewGuid(),
+                            Nome = model.Nome,
+                            Email = model.Email,
+                            Senha = SHA1Helper.Encrypt(model.Senha),
+                            DataHoraCriacao = DateTime.Now
+                        };
+
+                        //gravar o usuário no banco de dados                    
+                        usuarioRepository.Create(usuario);
+
+                        TempData["MensagemSucesso"] = $"Parabéns {usuario.Nome}, sua conta de usuário foi criada com sucesso.";
+                        ModelState.Clear();
+                    }                    
+                }
+                catch(Exception e)
+                {
+                    TempData["MensagemErro"] = e.Message;
+                }
+            }
+            else
+            {
+                TempData["MensagemAlerta"] = "Ocorreram erros de validação no preenchimento do formulário, por favor verifique.";
+            }
+
             return View();
         }
 
---
+++ FinancasApp.Presentation/Models/AccountRegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinancasApp.Presentation.Models
+{
+    /// <summary>
+    /// Modelo de dados para a página /Account/Register
+    /// </summary>
+    public class AccountRegisterViewModel
+    {
+        [MinLength(8, ErrorMessage = "Por favor, informe no mínimo {1} caracteres.")]
+        [MaxLength(100, ErrorMessage = "Por favor, informe no máximo {1} caracteres.")]
+        [Required(ErrorMessage = "Por favor, informe o nome do usuário.")]
+        public string Nome { get; set; }
+
+        [EmailAddress(ErrorMessage = "Por favor, informe um endereço de email válido.")]
+        [Required(ErrorMessage = "Por favor, informe o email do usuário.")]
+        public string Email { get; set; }
+
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=]).{8,}$", 
+            ErrorMessage = "Por favor, informe a senha com letras maiúsculas, minúsculas, números, símbolos e pelo menos 8 caracteres.")]
+        [Required(ErrorMessage = "Por favor, informe a senha do usuário.")]
+        public string Senha { get; set; }
+
+        [Compare("Senha", ErrorMessage = "Senhas não conferem, por favor verifique.")]
+        [Required(ErrorMessage = "Por favor, confirme a senha do usuário.")]
+        public string SenhaConfirmacao { get; set; }
+    }
+}
